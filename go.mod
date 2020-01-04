2be538d4d94dd58d290865be423494ef623c1181
tzongw <tzongw@gmail.com>
2020-01-04 21:47:26 +0800

    test

--- go.mod
+++ go.mod
@@ -2,8 +2,14 @@ module registry
 
 go 1.13
 
+replace shared v0.0.0 => /Users/tangzongwei/Documents/thrift/tutorial/gen-go/shared
+
+replace tutorial v0.0.0 => /Users/tangzongwei/Documents/thrift/tutorial/gen-go/tutorial
+
 require (
 	github.com/apache/thrift v0.13.0
 	github.com/go-redis/redis v6.15.6+incompatible
 	github.com/sirupsen/logrus v1.4.2
+	shared v0.0.0 // indirect
+	tutorial v0.0.0
 )
--- main.go
+++ main.go
@@ -1,15 +1,79 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/go-redis/redis"
 	log "github.com/sirupsen/logrus"
+	"tutorial"
 )
 
 func main() {
-	log.SetLevel(log.DebugLevel)
+	log.SetLevel(log.ErrorLevel)
 	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
 	client := redis.NewClient(&redis.Options{})
 	s := NewService(client)
 	s.Start(map[string]string{"aa": "bb", "cc": "dd"})
+	test()
 	select {}
+}
+
+func test() {
+	c := NewClient("127.0.0.1:19090", nil)
+	cli := tutorial.NewCalculatorClient(c)
+	for i := 0; i < 10; i += 1 {
+		go handleClient(cli)
+	}
+}
+
+var defaultCtx = context.Background()
+
+func handleClient(client *tutorial.CalculatorClient) (err error) {
+	client.Ping(defaultCtx)
+	fmt.Println("ping()")
+
+	sum, _ := client.Add(defaultCtx, 1, 1)
+	fmt.Print("1+1=", sum, "\n")
+
+	work := tutorial.NewWork()
+	work.Op = tutorial.Operation_DIVIDE
+	work.Num1 = 1
+	work.Num2 = 0
+	quotient, err := client.Calculate(defaultCtx, 1, work)
+	if err != nil {
+		switch v := err.(type) {
+		case *tutorial.InvalidOperation:
+			fmt.Println("Invalid operation:", v)
+		default:
+			fmt.Println("Error during operation:", err)
+		}
+		return err
+	} else {
+		fmt.Println("Whoa we can divide by 0 with new value:", quotient)
+	}
+
+	work.Op = tutorial.Operation_SUBTRACT
+	work.Num1 = 15
+	work.Num2 = 10
+	diff, err := client.Calculate(defaultCtx, 1, work)
+	if err != nil {
+		switch v := err.(type) {
+		case *tutorial.InvalidOperation:
+			fmt.Println("Invalid operation:", v)
+		default:
+			fmt.Println("Error during operation:", err)
+		}
+		return err
+	} else {
+		fmt.Print("15-10=", diff, "\n")
+	}
+
+	log, err := client.GetStruct(defaultCtx, 1)
+	if err != nil {
+		fmt.Println("Unable to get struct:", err)
+		return err
+	} else {
+		fmt.Println("Check log:", log.Value)
+	}
+	return err
 }
