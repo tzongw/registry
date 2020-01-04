5f4ef90e620116904ac4f7c89b5cec62a38e945d
tzongw <tzongw@gmail.com>
2020-01-04 21:00:08 +0800

    client

--- thrift_pool.go
+++ thrift_pool.go
@@ -1,42 +1,73 @@
 package main
 
 import (
+	"context"
 	"github.com/apache/thrift/lib/go/thrift"
 )
 
-type thriftPool struct {
-	addr string
-	transportFactory thrift.TTransportFactory
-	protocolFactory thrift.TProtocolFactory
+type client struct {
+	*thrift.TStandardClient
+	trans thrift.TTransport
 }
 
-func NewThriftPool(addr string) *thriftPool  {
-	return &thriftPool{
-		addr:addr,
-		transportFactory : thrift.NewTBufferedTransportFactory(8192),
-		protocolFactory : thrift.NewTBinaryProtocolFactoryDefault(),
+type Client struct {
+	p *pool
+}
+
+func NewClient(addr string, opt *Options) *Client {
+	return &Client{
+		p: NewPool(NewThriftFactory(addr), opt),
 	}
 }
 
-func (p *thriftPool) Open() (interface{}, error) {
+func (c *Client) Call(ctx context.Context, method string, args, result thrift.TStruct) error {
+	cc, err := c.p.Get()
+	if err != nil {
+		return err
+	}
+	err = cc.(*client).Call(ctx, method, args, result)
+	switch err.(type) {
+	case thrift.TException:
+		c.p.Put(cc, err)
+	default:
+		c.p.Put(cc, nil)
+	}
+	return err
+}
+
+type thriftFactory struct {
+	addr             string
+	transportFactory thrift.TTransportFactory
+	protocolFactory  thrift.TProtocolFactory
+}
+
+func NewThriftFactory(addr string) *thriftFactory {
+	return &thriftFactory{
+		addr:             addr,
+		transportFactory: thrift.NewTBufferedTransportFactory(8192),
+		protocolFactory:  thrift.NewTBinaryProtocolFactoryDefault(),
+	}
+}
+
+func (t *thriftFactory) Open() (interface{}, error) {
 	var transport thrift.TTransport
-	transport, err := thrift.NewTSocket(p.addr)
+	transport, err := thrift.NewTSocket(t.addr)
 	if err != nil {
 		return nil, err
 	}
-	transport, err = p.transportFactory.GetTransport(transport)
+	transport, err = t.transportFactory.GetTransport(transport)
 	if err != nil {
 		return nil, err
 	}
 	if err := transport.Open(); err != nil {
 		return nil, err
 	}
-	return p.protocolFactory.GetProtocol(transport), nil
+	iprot := t.protocolFactory.GetProtocol(transport)
+	oprot := t.protocolFactory.GetProtocol(transport)
+	return &client{TStandardClient: thrift.NewTStandardClient(iprot, oprot), trans: transport}, nil
 }
 
-func (p *thriftPool) Close(conn interface{}) error {
-	trans := conn.(thrift.TProtocol)
-	return trans.Transport().Close()
+func (t *thriftFactory) Close(conn interface{}) error {
+	c := conn.(*client)
+	return c.trans.Close()
 }
-
-
