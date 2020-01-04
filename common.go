dc838fa8f956978b251d3e6094379cc8e40345da
tzongw <tzongw@gmail.com>
2020-01-05 05:09:30 +0800

    common

---
+++ common.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/go-redis/redis"
+	"registry/gen-go/service"
+)
+
+var (
+	registry   = NewRegistry(redis.NewClient(&redis.Options{}))
+	userClient = service.NewUserClient(NewServiceClient(registry, RpcUser, nil))
+	gateClient = service.NewGateClient(NewServiceClient(registry, RpcGate, nil))
+)
