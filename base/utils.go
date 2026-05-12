3d0570c2622492ffa756428f0f432e1f4911f445
tzongw <tzongw@gmail.com>
2026-05-12 14:34:56 +0800

    utils

--- base/utils.go
+++ base/utils.go
@@ -2,17 +2,45 @@ package base
 
 import (
 	"net"
+	"strings"
 	"unsafe"
 )
 
-func StringToBytes(s string) []byte {
-	return unsafe.Slice(unsafe.StringData(s), len(s))
+var (
+	LocalIP string
+)
+
+func init() {
+	ip := net.ParseIP("8.8.8.8")
+	dstAddr := &net.UDPAddr{IP: ip, Port: 9}
+	conn, err := net.DialUDP("udp", nil, dstAddr)
+	if err != nil {
+		panic(err)
+	}
+	addr := conn.LocalAddr().String()
+	ss := strings.SplitN(addr, ":", 2)
+	LocalIP = ss[0]
+	_ = conn.Close()
+}
+
+// ExtractHost returns a real ip
+func ExtractHost(host string) (string, error) {
+	// if host specified then its returned
+	if len(host) > 0 && host != "0.0.0.0" && host != "[::]" && host != "::" {
+		return host, nil
+	}
+
+	return LocalIP, nil
 }
 
 func HostPort(hp string) (host, port string, err error) {
 	if host, port, err = net.SplitHostPort(hp); err != nil {
 		return
 	}
-	host, err = Extract(host)
+	host, err = ExtractHost(host)
 	return
+}
+
+func StringToBytes(s string) []byte {
+	return unsafe.Slice(unsafe.StringData(s), len(s))
 }
