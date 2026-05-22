ac22cea8b6f03830659bac1f785ef2a12e7fc445
tzongw <tzongw@gmail.com>
2026-05-22 22:45:17 +0800

    all join

--- bench/bench.go
+++ bench/bench.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/url"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -14,13 +15,14 @@ import (
 var addr = flag.String("addr", ":18080", "http service address")
 var start = flag.Int("start", 0, "start uid")
 var count = flag.Int("count", 1000, "uid count")
-var tick = flag.Int("tick", 10, "msg tick")
+var tick = flag.Int("tick", 100, "msg tick")
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
 	wg := sync.WaitGroup{}
 	wg.Add(*count)
+	var joinCount atomic.Int64
 	groupTick := time.NewTicker(time.Duration(*tick) * time.Millisecond)
 	defer groupTick.Stop()
 	for i := range *count {
@@ -35,6 +37,8 @@ func main() {
 			c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 			if err != nil {
 				log.Println("dial:", err)
+				// 连接失败也计入 joinCount，避免其余连接因此永远等待
+				joinCount.Add(1)
 				return
 			}
 			defer c.Close()
@@ -50,10 +54,15 @@ func main() {
 					}
 				}
 			}()
+
 			pingTick := time.NewTicker(45 * time.Second)
 			defer pingTick.Stop()
 			joined := false
 			for {
+				var ch <-chan time.Time
+				if !joined || joinCount.Load() == int64(*count) {
+					ch = groupTick.C
+				}
 				select {
 				case <-pingTick.C:
 					err := c.WriteMessage(websocket.PingMessage, []byte{})
@@ -61,7 +70,7 @@ func main() {
 						log.Println(uid, " write: ", err)
 						return
 					}
-				case <-groupTick.C:
+				case <-ch:
 					var msg string
 					if joined {
 						msg = fmt.Sprintf("hello %d", uid)
@@ -76,6 +85,7 @@ func main() {
 					}
 					if !joined {
 						joined = true
+						joinCount.Add(1)
 						time.Sleep(100 * time.Millisecond)
 					}
 				}
